354a603786eb571672e3830623d89e405d1442e4
David Battye <david2.battye@live.uwe.ac.uk>
2019-10-07 16:45:49 +0100

    First todo commit make initial todos array import lots material-ui

---
+++ src/TodoApp.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+// import Grid from '@material-ui/core/Grid';
+
+function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: "Clean fishtank", completed: false },
+    { id: 2, task: "Wash Car", completed: true },
+    { id: 3, task: "Grow Beard", completed: false }
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+  return (
+    <Paper style={{ // paper gives a background
+      padding: 0,
+      margin: 0,
+      height: "100vh",
+      backgroundColor: "#fafafa"}}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">Todos with Hooks</Typography>
+        </Toolbar>
+        <TodoList todos={todos} />
+      </AppBar>
+    </Paper>
+  );
+}
+
+export default TodoApp;
+
+// - TodoApp
+//    - TodoForm
+//    - TodoList
+//       -TodoItem
+
+// each Todo will have an id, task, completed
